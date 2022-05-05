b3b1c5155500632b51d5a6a34f57b8a4c5e2a8c4
nurba001 <amanbekovnurbek04@gmail.com>
2022-05-05 23:53:36 +0600

    oop

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Triangle triangle=new Triangle();
+        triangle.a=3;
+        triangle.b=3;
+        triangle.c=5;
+            triangle.area();
+    }
+}
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Triangle {
+    int a,b,c;
+ void  area(){
+        int s=a+b+c;
+        System.out.println("Area= "+s);
+    }
+}
